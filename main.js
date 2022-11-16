e07937c3829183b3618938c4d1929db29517d80a
rafa <homer512x@gmail.com>
2022-11-16 14:37:39 +0100

    q

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 //Crea un array de 3 pokemons, el tipo de dato a usar será un objeto. Deberá contener nombre, tipoDePokemon.
 
 var pokemons = [
-	(pokemon1 = {
+	{
 		nombre: "Charmander",
 		tipoDePokemon: "Fuego",
-	}),
-	(pokemon2 = {
+	},
+	{
 		nombre: "Pikachu",
 		tipoDePokemon: "Electrico",
-	}),
-	(pokemon3 = {
+	},
+	{
 		nombre: "Bulbasaur",
 		tipoDePokemon: "Veneno",
-	}),
+	},
 ]
 
 //Crea una condición y si el tipo de pokemon es fuego muestra por consola el mensaje ¡Es un pokemon de fuego!.
